864ec1d1204b50b4373ea22213579ec97e3a025f
[suraj] <[suraj1996xyz@gmail.com]>
2020-10-16 23:42:44 +0530

    Updates

---
+++ precache-manifest.adaf60e818fdeee982be3c5b598f6a11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997ccd25eb9f9b471ecc5cc1d3d06eaa",
+    "url": "/reactform/index.html"
+  },
+  {
+    "revision": "6708e6f31e3c724e9c1f",
+    "url": "/reactform/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e5dbd12f7d62abd3e6c8",
+    "url": "/reactform/static/css/main.a5a7feeb.chunk.css"
+  },
+  {
+    "revision": "6708e6f31e3c724e9c1f",
+    "url": "/reactform/static/js/2.1231350f.chunk.js"
+  },
+  {
+    "revision": "837d3f4b36c68814ee561a861ebbda11",
+    "url": "/reactform/static/js/2.1231350f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5dbd12f7d62abd3e6c8",
+    "url": "/reactform/static/js/main.cb831c50.chunk.js"
+  },
+  {
+    "revision": "2a065780cac0187d2d4b",
+    "url": "/reactform/static/js/runtime-main.56e5576b.js"
+  },
+  {
+    "revision": "ae47b47b3a094f52423ce51409a1c75e",
+    "url": "/reactform/static/media/img.ae47b47b.png"
+  }
+]);
